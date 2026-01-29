2ee396b45c4a08a27314bf77683dfaf6dbfa085b
Zach Kohlberg <zach.kohlberg@gmail.com>
2026-01-29 13:20:08 -0500

    update week 2-3 outlines and schedule

--- csc111/searcher-c2a407aa.js
+++ csc111/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-99e734e8.js',
+                path_to_root + 'searchindex-4923379f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
