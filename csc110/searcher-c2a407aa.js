26c887bd10507c7f252363debc91171c9610e17d
Zach Kohlberg <zach.kohlberg@gmail.com>
2026-02-05 13:03:20 -0500

    update websites

--- csc110/searcher-c2a407aa.js
+++ csc110/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1e704c2a.js',
+                path_to_root + 'searchindex-d0693b7c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
