560c923a821280e4d7998ce1411b1b53f059312b
Zach Kohlberg <zach.kohlberg@gmail.com>
2026-02-10 13:03:19 -0500

    notes for 2/10

--- csc110/searcher-c2a407aa.js
+++ csc110/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d0693b7c.js',
+                path_to_root + 'searchindex-3576e050.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
