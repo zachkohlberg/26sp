8fa1d21810ba7943a465f4360f7c83653bd6fdf3
Zach Kohlberg <zach.kohlberg@gmail.com>
2026-02-11 19:01:16 -0500

    update w4 schedule

--- csc110/searcher-c2a407aa.js
+++ csc110/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9e1220f5.js',
+                path_to_root + 'searchindex-06d09059.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- csc111/searcher-c2a407aa.js
+++ csc111/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-61fea0ab.js',
+                path_to_root + 'searchindex-a30ae0dc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
