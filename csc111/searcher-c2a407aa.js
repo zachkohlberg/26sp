6377ad8494b0012eaeeb8c949b9f253e24057634
Zach Kohlberg <zach.kohlberg@gmail.com>
2026-02-18 13:35:42 -0500

    add missing Circle code to csc111 w4 examples

--- csc111/searcher-c2a407aa.js
+++ csc111/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ca1c4ca5.js',
+                path_to_root + 'searchindex-f7b27262.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
