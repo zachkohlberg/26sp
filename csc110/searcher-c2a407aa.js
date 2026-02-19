659ab63f66c870ec889ab687a6c4ef0a25114b0c
Zach Kohlberg <zach.kohlberg@gmail.com>
2026-02-19 10:35:30 -0500

    add csc110 assignment, some fixes to csc111 assignment and schedule

--- csc110/searcher-c2a407aa.js
+++ csc110/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-18bc2e27.js',
+                path_to_root + 'searchindex-bac4e74b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
