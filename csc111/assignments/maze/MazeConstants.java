3af327d98cec00611e3d0d2546614bde5f1f5c27
Zach Kohlberg <zach.kohlberg@gmail.com>
2026-02-28 19:08:53 -0500

    finish maze assignment

---
+++ csc111/assignments/maze/MazeConstants.java
@@ -0,0 +1,7 @@
+public class MazeConstants {
+    public static final char EMPTY = ' ';
+    public static final char PATH = '.';
+    public static final char START = 'S';
+    public static final char VISITED = 'v';
+    public static final char WALL = '#';
+}
