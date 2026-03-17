d38f49d6226bebb11d21ad7395e845d4d3ce46d4
Zach Kohlberg <zach.kohlberg@gmail.com>
2026-03-16 21:11:48 -0400

    update websites for week 8

--- csc111/searcher-c2a407aa.js
+++ csc111/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e5397cfd.js',
+                path_to_root + 'searchindex-d94d6f58.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
