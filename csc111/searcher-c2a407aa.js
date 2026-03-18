181bfcdcd1f1a6b1f3c9fae0ca12d35347066019
Zach Kohlberg <zach.kohlberg@gmail.com>
2026-03-18 16:25:55 -0400

    update assignment policy and assignment point values

--- csc111/searcher-c2a407aa.js
+++ csc111/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d94d6f58.js',
+                path_to_root + 'searchindex-8b70be83.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
