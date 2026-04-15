c6c163609decfc22e4df34748dc2e63455cc163c
Zach Kohlberg <zach.kohlberg@gmail.com>
2026-04-15 15:49:50 -0400

    final project introduction

--- csc111/searcher-c2a407aa.js
+++ csc111/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-599e0fe5.js',
+                path_to_root + 'searchindex-d0eb7efe.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
