171d562f3239a8e14e8b1d97e015b475bfbca07f
Zach Kohlberg <zach.kohlberg@gmail.com>
2026-04-29 17:45:58 -0400

    update maze assignment with pseudocode

--- csc111/searcher-c2a407aa.js
+++ csc111/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9f70248a.js',
+                path_to_root + 'searchindex-500944cb.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
